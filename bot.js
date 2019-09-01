0f2cfa790b5cba184556208b38c90e24b718e739
Matter51 <Matt.rosenfeld@icloud.com>
2019-09-01 19:25:29 +0100

    added joining message

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
-
+const GENERALCHATID = '617737877840986165';
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg =>{
+client.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('pong');
+    }
+});
 
+client.on('guildMemberAdd', member => {
+    const newUser = member.user.username;
+    console.log('user joining server: ', newUser);
+    if (newUser === 'xandermonkey10') {
+        member.guild.channels.get(GENERALCHATID).send("u da man");
     }
 });
 
